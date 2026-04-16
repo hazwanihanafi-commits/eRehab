c13068723e3271a3286e4ce57dcf6c698d6de194
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-04-16 23:32:12 +0800

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="bg-gradient-to-r from-blue-700 to-blue-500 text-white p-4 rounded mb-6">
+      <h1 className="text-xl font-bold">
+        e-Rehab Clinical System
+      </h1>
+      <p className="text-sm">
+        Developed by Hazwani Ahmad Yusof, Pusat Kanser Tun Abdullah Ahmad Badawi, USM
+      </p>
+    </div>
+  );
+}
